8c9a790c2946b45b80705a18522d82bdcfd15dae
Alberto Marín <macramacramacra@gmail.com>
2025-09-26 12:33:39 +0200

    Implementación de la lógica del programa

---
+++ Ejercicio06/Ejercicio06.py
@@ -0,0 +1,21 @@
+"""
+Un vendedor recibe un sueldo base mas un 10% 
+extra por comisión de sus ventas, el vendedor desea 
+saber cuanto dinero obtendrá por concepto de comisiones 
+por las tres ventas que realiza en el mes y el total que 
+recibirá en el mes tomando en cuenta su sueldo base y comisiones
+"""
+
+sueldoBase = float(input("Ingrese su sueldo base: "))
+numVentas: int = 3
+
+ventas = []
+for i in range(1, numVentas + 1):
+    venta = float(input(f"Ingrese el monto de la venta {i}: "))
+    ventas.append(venta)
+
+percepcionComision = sum(venta * 0.10 for venta in ventas)  # 10% de cada venta
+sueldoTotal = sueldoBase + percepcionComision
+
+print(f"-> Percepción por comisiones: {percepcionComision}")
+print(f"-> El sueldo total es: {sueldoTotal}")
