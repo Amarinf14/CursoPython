434eb07357d54261ad622c079cbe65b33a13e289
Alberto Marín <macramacramacra@gmail.com>
2025-11-04 10:28:25 +0100

    Añade función suma(a, b)

---
+++ Unidad02/HojaEjercicios01/ej02_suma.py
@@ -0,0 +1,10 @@
+"""
+Suma con parámetros. Escribe una función
+suma(a,b) que devuelva la suma de dos números.
+"""
+def suma(a: float, b:float) -> float:
+    return a + b
+
+# Ejemplo de uso
+print(suma(3, 5))  # Salida: 8
+print(suma(3.5, 2.7))  # Salida: 6.2
