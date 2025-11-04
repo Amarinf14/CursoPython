cbc9aa3bcdf3770795551f74ed5f88d7dd6618d0
Alberto Marín <macramacramacra@gmail.com>
2025-11-04 10:21:09 +0100

    Añade función saludar(nombre) con saludo personalizado

---
+++ Unidad02/HojaEjercicios01/ej01_saludador.py
@@ -0,0 +1,10 @@
+"""
+Saludo personalizado. 
+Define una función saludar(nombre) 
+que reciba un nombre y retorne un saludo.
+"""
+def saludar(nombre: str) -> str:
+    return f"Hola, {nombre}!"
+
+# Ejemplo de uso
+print(saludar("Marín"))
